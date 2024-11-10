213716866855eb0d785ea4d197fba20927cef8e5
emmanuel okinyi <emmanuelokinyi799@gmail.com>
2024-11-10 04:47:29 +0300

    adds gsap animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,32 +1,115 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import styles from "./Hero.module.css";
-import Spline from '@splinetool/react-spline';
 import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      `.${styles.headshot}`,
+      {
+        opacity: 0,
+        y: 100
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.5,
+        scrollTrigger: {
+          trigger: `.${styles.container}`,
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true
+        }
+      }
+    );
+
+    gsap.fromTo(
+      `.${styles.title}`,
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.2,
+        delay: 0.2,
+        scrollTrigger: {
+          trigger: `.${styles.container}`,
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true
+        }
+      }
+    );
+    gsap.fromTo(
+      `.${styles.description}`,
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.2,
+        delay: 0.4,
+        scrollTrigger: {
+          trigger: `.${styles.container}`,
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true
+        }
+      }
+    );
+
+  
+    gsap.fromTo(
+      `.${styles.topBlur}`,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: `.${styles.container}`,
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true
+        }
+      }
+    );
+
+    gsap.fromTo(
+      `.${styles.bottomBlur}`,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: `.${styles.container}`,
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true
+        }
+      }
+    );
+  }, []);
+
   return (
-    <>
-      <Spline scene="https://prod.spline.design/bDQgb1moh719Avab/scene.splinecode" />
-      <section className={styles.container}>
-        <div className={styles.imageContainer}>
-          <img
-            className={styles.headshot}
-            src={getImageUrl("hero/me.png")}
-            alt="Emmanuel Okinyi"
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Emmanuel Okinyi</h1>
-          <h2 className={styles.description}>
-            is a full-stack developer with 2 years of experience using HTML, CSS, JavaScript, and React.
-            I am passionate about tech, ready to dive into new projects with amazing software solutions.
-            Feel free to reach out to learn more.
-          </h2>
-          <a className={styles.contactBtn} href="mailto:emmanuelokinyi799@gmail.com">Contact me</a>
-        </div>
-        <div className={styles.topBlur}></div>
-        <div className={styles.bottomBlur}></div>
-      </section>
-    </>
+    <section className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.headshot}
+          src={getImageUrl("hero/me.png")}
+          alt="Emmanuel Okinyi"
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Emmanuel Okinyi</h1>
+        <h2 className={styles.description}>
+          I'm a full-stack developer with 2 years of experience using JavaScript, and Python as my primary programming languages.
+          I am passionate about tech, ready to dive into new projects with amazing software solutions.
+          Feel free to reach out to learn more.
+        </h2>
+        <a className={styles.contactBtn} href="mailto:emmanuelokinyi799@gmail.com">Contact me</a>
+      </div>
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
+    </section>
   );
 };
